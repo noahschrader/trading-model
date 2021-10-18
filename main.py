744a5f930878456192a56edc72f79b87e89dcec2
Noah Schrader <noah.r.schrader@gmail.com>
2021-10-18 14:21:02 -0500

    output accuracies and predictions for knn model

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pandas
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import GridSearchCV, cross_val_score
+from sklearn.model_selection import GridSearchCV, cross_val_score, cross_val_predict
 from sklearn.preprocessing import StandardScaler
 
 dataset = pandas.read_csv('train.csv')
@@ -16,20 +16,31 @@ train = StandardScaler().fit_transform(train)
 
 # needed to tune n_neighbors parameter for knn
 # n_neighbors = [{"n_neighbors": [4, 6, 8, 10, 12]}]  8
-# meta = GridSearchCV(knn, n_neighbors, cv=5, scoring="neg_root_mean_squared_error", refit=True)
+# meta = GridSearchCV(knn, n_neighbors, cv=5, scoring="roc_auc", refit=True)
 # meta.fit(train, target)
 # print(meta.best_params_)
 
 knn = KNeighborsClassifier()
-knn_score = cross_val_score(knn, train, target, cv=10, scoring="roc_auc")
-print(knn_score.mean())
+knn_scores = cross_val_score(knn, train, target, cv=10, scoring='roc_auc')
+knn_accuracy = knn_scores.mean()
+knn_predictions = cross_val_predict(knn, train, target, cv=10)
 
 lr = LogisticRegression()
-lr.fit(train, target)
-lr_score = cross_val_score(lr, train, target, cv=10, scoring="roc_auc")
-print(lr_score.mean())
+lr_scores = cross_val_score(lr, train, target, cv=10, scoring='roc_auc')
+lr_accuracy = lr_scores.mean()
+lr_predictions = cross_val_predict(lr, train, target, cv=10)
 
 nb = GaussianNB()
-nb.fit(train, target)
-nb_score = cross_val_score(nb, train, target, cv=10, scoring="roc_auc")
-print(nb_score.mean())
+nb_scores = cross_val_score(nb, train, target, cv=10, scoring='roc_auc')
+nb_accuracy = nb_scores.mean()
+nb_predictions = cross_val_predict(nb, train, target, cv=10)
+
+print('--------Accuracy Scores--------')
+print('K-Nearest Neighbors: ' + str(round(knn_accuracy * 100, 4)) + ' %')
+print('Logistic Regression: ' + str(round(lr_accuracy * 100, 4)) + ' %')
+print('Naive Bayes: ' + str(round(nb_accuracy * 100, 4)) + ' %')
+
+# knn model performs the best so format its results
+actions = pandas.DataFrame(nb_predictions, columns=['Action'])
+results = pandas.concat([dataset['ts_id'], actions], axis=1)
+results.to_csv(path_or_buf='results.csv')
